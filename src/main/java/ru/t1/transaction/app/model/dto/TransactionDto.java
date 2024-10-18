6862d3847a54543aaa657dd4daeb18050725acd1
BlackPol84 <orlovpol84@gmail.com>
2024-10-18 22:55:13 +0300

    added a ExceptionController

--- src/main/java/ru/t1/transaction/app/model/dto/TransactionDto.java
+++ src/main/java/ru/t1/transaction/app/model/dto/TransactionDto.java
@@ -2,6 +2,7 @@ package ru.t1.transaction.app.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,14 +17,18 @@ import java.math.BigDecimal;
 public class TransactionDto {
 
     @JsonProperty("amount")
+    @NotNull(message = "Amount should not be null")
     private BigDecimal amount;
 
     @JsonProperty("client_id")
+    @NotNull(message = "Client ID should not be null")
     private Long clientId;
 
     @JsonProperty("account_id")
+    @NotNull(message = "Account ID should not be null")
     private Long accountId;
 
     @JsonProperty("transaction_type")
+    @NotNull(message = "Transaction type should not be null")
     private TransactionType transactionType;
 }
