0c9335c7113a55cb425268d26455133ce15cb188
BlackPol84 <orlovpol84@gmail.com>
2024-10-30 18:39:27 +0300

    modified an application.yml

--- src/test/java/ru/t1/transaction/app/controller/TransactionControllerTest.java
+++ src/test/java/ru/t1/transaction/app/controller/TransactionControllerTest.java
@@ -13,12 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.t1.transaction.app.kafka.TransactionKafkaProducer;
-import ru.t1.transaction.app.model.dto.TransactionDto;
 import ru.t1.transaction.app.model.TransactionType;
+import ru.t1.transaction.app.model.dto.TransactionDto;
 
 import java.math.BigDecimal;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
